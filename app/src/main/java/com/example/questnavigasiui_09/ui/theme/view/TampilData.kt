c7a5f4e7b9b5a33dc0505c15740591c2807eef33
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-11-06 15:37:07 +0700

    menambahkan isiruang dengan column

--- app/src/main/java/com/example/questnavigasiui_09/ui/theme/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_09/ui/theme/view/TampilData.kt
@@ -1,5 +1,15 @@
 package com.example.questnavigasiui_09.ui.theme.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -8,22 +18,35 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigasiui_09.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()-> Unit
-){
+) {
     val items = listOf(
         Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
-        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
-    )
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta"),
+
+        )
     Scaffold (modifier = Modifier,
         {
             TopAppBar(
                 title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
-                colors = TopAppBarDefaults
-                    .mediumTopAppBarColors(colorResource(id=R.color.teal_700))
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id = R.color.teal_700))
             )
+        }){ isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column (modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))){
+                items.forEach {item ->
+
